94723bc1f08e019e463b20f9dcf53dbd9b1995fc
Dmitry Serebrennikov <dimafanya@gmail.com>
2020-12-11 12:46:48 +0200

    Remove last line

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 'use strict';
 
-function unluckyDays(year) {
-  let date = new Date(year, 0),
-    count = 0;
-  while (date.getFullYear() < year + 1) {
-    if (date.getDay() == 5 && date.getDate() == 13) count++;
-    date.setDate(date.getDate() + 1);
-  }
-  return count;
-}
+const desert = { type: 'pie' };
+desert.type = 'gdfg';
